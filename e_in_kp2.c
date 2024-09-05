3002c7edf9852a7ea42454d6682ccf5709499da5
SAGAR T <ssagu6661@gmail.com>
2024-09-05 11:43:32 -0400

    Add files via upload

---
+++ e_in_kp2.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+
+int a=10;                                      /*Arithmetic operators*/
+int b=8; 
+printf("sum of %d\n",a+b );                    /*simple output program*/
+
+
+// a--;                                         /*DICREMENTAL OPERATORS*/
+// printf("dicrement %d\n",a );
+
+// a++;                                         /*INCREMENTAL OPERATOR*/
+// printf("increment %d\n",a );
+
+// a+=20;                                         /*ASSIGNMENT OPERATOR*/
+// printf("Assignment operetor %d\n",a );
+
+//  printf("%d\n",a!=b );           /*RELATION OPERATOR*/      /*when it is equal. it gives the  output  1,,,*/
+                                                                /*when it is not equal. the output is 0.*/
+                                                                 
+
+// printf("%d\n", a==b && a% 2==0);       /*LOGICAL OPERATOR*/                        /*when it is equal. it gives the  output  1,,,*/
+                                                                /*when it is not equal. the output is 0.*/
+
+
+printf("%d", a&b);                      /*BITWISE OPERATOR*/ 
+
+    return 0;
+
+}
